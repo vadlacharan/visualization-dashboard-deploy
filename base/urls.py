b6e0b678164338bf8d5fb60b8c417506dcd7cc79
vadlacharan <146012419+vadlacharan@users.noreply.github.com>
2024-06-30 22:17:01 +0530

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('data-table-filter/',views.data_table_filter,name="data_table_filter"),
 
    
-    #path('save_data/',views.save_data,name="save_data")
+    path('save_data/',views.save_data,name="save_data")
 ]
